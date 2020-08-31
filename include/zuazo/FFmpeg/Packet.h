97c783e8d74bdd0ada4cf6378fbbc2c3b4beff6f
Oier Lauzirika Zarrabeitia <oierlauzi@riseup.net>
2020-08-31 19:23:02 +0200

    Added a façade class for AVPacket

---
+++ include/zuazo/FFmpeg/Packet.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <zuazo/Utils/BufferView.h>
+
+#include <cstddef>
+
+struct AVPacket;
+
+namespace Zuazo::FFmpeg {
+
+class Packet {
+public:
+	using Handle = AVPacket*;
+	using ConstHandle = const AVPacket*;
+
+	Packet();
+	Packet(size_t size);
+	Packet(ConstHandle hand);
+	Packet(const Packet& other);
+	Packet(Packet&& other);
+	~Packet();
+
+	Packet& 							operator=(const Packet& other);
+	Packet&								operator=(Packet&& other);
+
+	operator Handle();
+	operator ConstHandle() const;
+
+	void								swap(Packet& other);
+
+	void								setPTS(int64_t pts);
+	int64_t								getPTS() const;
+
+	void								setDTS(int64_t dts);
+	int64_t								getDTS() const;
+
+	void								setDuration(int64_t duration);
+	int64_t								getDuration() const;
+	
+	void								setStreamIndex(int idx);
+	int									getStreamIndex() const;
+
+	Utils::BufferView<std::byte> 		getData();
+	Utils::BufferView<const std::byte>	getData() const;
+
+	void								shrink(size_t size);
+	void								grow(size_t size);
+
+private:
+	Handle								m_handle;
+};
+
+}
---
+++ src/FFmpeg/Packet.cpp
@@ -0,0 +1,138 @@
+#include <zuazo/FFmpeg/Packet.h>
+
+extern "C" {
+	#include <libavcodec/packet.h>
+}
+
+namespace Zuazo::FFmpeg {
+
+Packet::Packet() 
+	: m_handle(av_packet_alloc())
+{
+}
+
+Packet::Packet(size_t size) 
+	: Packet() 
+{
+	av_new_packet(m_handle, size);
+}
+
+Packet::Packet(ConstHandle hand) 
+	: m_handle(av_packet_clone(hand))
+{
+}
+
+
+Packet::Packet(const Packet& other)
+	: Packet(other.m_handle)
+{
+}
+
+Packet::Packet(Packet&& other) 
+	: m_handle(other.m_handle)
+{
+	other.m_handle = nullptr;
+}
+
+Packet::~Packet() {
+	//nullptr can be passed
+	av_packet_free(&m_handle);
+}
+
+
+
+Packet& Packet::operator=(const Packet& other) {
+	Packet(other).swap(*this);
+	return *this;
+}
+
+Packet& Packet::operator=(Packet&& other) {
+	Packet(std::move(other)).swap(*this);
+	return *this;
+}
+
+
+
+Packet::operator Handle() {
+	return m_handle;
+}
+
+Packet::operator ConstHandle() const {
+	return m_handle;
+}
+
+
+
+void Packet::swap(Packet& other) {
+	std::swap(m_handle, other.m_handle);
+}
+
+
+
+void Packet::setPTS(int64_t pts) {
+	assert(m_handle);
+	m_handle->pts = pts;
+}
+
+int64_t Packet::getPTS() const {
+	assert(m_handle);
+	return m_handle->pts;
+}
+
+
+
+void Packet::setDTS(int64_t dts) {
+	assert(m_handle);
+	m_handle->dts = dts;
+}
+
+int64_t Packet::getDTS() const {
+	assert(m_handle);
+	return m_handle->dts;
+}
+
+
+
+void Packet::setDuration(int64_t duration) {
+	assert(m_handle);
+	m_handle->duration = duration; 
+}
+
+int64_t Packet::getDuration() const {
+	assert(m_handle);
+	return m_handle->duration; 
+}
+
+
+
+void Packet::setStreamIndex(int idx) {
+	assert(m_handle);
+	m_handle->stream_index = idx; 
+}
+
+int Packet::getStreamIndex() const {
+	assert(m_handle);
+	return m_handle->stream_index; 
+}
+
+
+
+Utils::BufferView<std::byte> Packet::getData() {
+	return Utils::BufferView<std::byte>(reinterpret_cast<std::byte*>(m_handle->data), m_handle->size);
+}
+
+Utils::BufferView<const std::byte>Packet::getData() const {
+	return Utils::BufferView<const std::byte>(reinterpret_cast<std::byte*>(m_handle->data), m_handle->size);
+}
+
+
+
+void Packet::shrink(size_t size) {
+	av_shrink_packet(m_handle, size);
+}
+
+void Packet::grow(size_t size) {
+	av_grow_packet(m_handle, size);
+}
+
+}
